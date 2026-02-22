6449405ea1ac7a438a22235ddd48f07dd9ff6ace
Mateus Filpo <mateusfilpo27@gmail.com>
2026-02-22 20:17:42 -0300

    chore: initial spring boot setup with neo4j configuration

---
+++ src/main/java/br/com/filpo/pokemart/PokemartApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.filpo.pokemart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemartApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemartApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/filpo/pokemart/PokemartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.filpo.pokemart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemartApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
