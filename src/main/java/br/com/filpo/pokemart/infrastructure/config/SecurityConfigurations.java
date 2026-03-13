5830834f4231b01099558d4f9198a6b3f9a96510
Mateus Filpo <mateusfilpo27@gmail.com>
2026-03-13 14:09:24 -0300

    fix(cors): permit domains em producao

--- src/main/java/br/com/filpo/pokemart/infrastructure/config/SecurityConfigurations.java
+++ src/main/java/br/com/filpo/pokemart/infrastructure/config/SecurityConfigurations.java
@@ -88,7 +88,8 @@ public class SecurityConfigurations {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                Arrays.asList("http://127.0.0.1:5500", "http://localhost:5500"));
+                Arrays.asList("http://127.0.0.1:5500", "http://localhost:5500", 
+                              "https://pokemart.filpo.com.br", "https://www.pokemart.filpo.com.br"));
 
         configuration.setAllowedMethods(
                 Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
